ca05459f94c0a0a6584ea8e358c7777c733d37ee
Unimke1990 <agimunimke1990@gmail.com>
2023-12-03 15:00:45 -0800

    update

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
-    if ord(str) >= 65 and ord(str) <= 90:
-        return True
-    else:
-        return False
+    for iterate in str:
+        temp = iterate
+        if ord(temp) >= 97 and ord(temp) <= 122:
+            temp = chr(ord(iterate) - 32)
+        print("{}".format(temp), ends='')
+    print()
