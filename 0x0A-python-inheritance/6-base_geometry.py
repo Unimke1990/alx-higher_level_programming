e8dd81b22d975b8d492c2bb63d3c53d9cab8ed96
Unimke1990 <agimunimke1990@gmail.com>
2024-01-16 12:42:22 -0800

    a class BaseGeometry that raises an exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""A BaseGeometry class"""
+
+
+class BaseGeometry:
+    """an area class method"""
+    def area(self):
+        """raises an exception"""
+        raise Exception("area() is not implemented")
