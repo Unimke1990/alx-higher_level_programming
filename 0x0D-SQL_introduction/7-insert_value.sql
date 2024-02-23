2d5ab82063ea80555a58bccfdc723205b1cc333b
Unimke1990 <agimunimke1990@gmail.com>
2024-02-23 05:22:00 -0800

    script that inserts a new row in the table in mysql

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,5 @@
+-- file: 7-insert_value.sql
+-- description: inserting a new row to a table
+
+-- script that inserts a new row to a table
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
