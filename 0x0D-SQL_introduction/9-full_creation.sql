467cb8837ee97aaaa68b8367d34066fb347e91b8
Unimke1990 <agimunimke1990@gmail.com>
2024-02-23 05:47:58 -0800

    script that creates a table with multiple rows

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -4,4 +4,4 @@
 -- script that creates a table with multiple rows
 CREATE TABLE IF NOT EXISTS second_table (id INTEGER AUTO_INCREMENT, name VARCHAR(256), score INTEGER);
 
-INSERT INTO second_table (name, score) VALUES (('John', 10), ('Alex', 3), ('Bob', 14), ('George', 8));
+INSERT INTO second_table (name, score) VALUES ('John', 10), ('Alex', 3), ('Bob', 14), ('George', 8);
