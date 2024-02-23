69f2c3c7d4d553a3675e26fffac02ee013f35ad9
Unimke1990 <agimunimke1990@gmail.com>
2024-02-23 04:46:40 -0800

    script that creates a table in a database

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,5 @@
+-- file: 4-first_table.sql
+-- description: creating a table in my current database
+
+-- script that creates a table
+CREATE TABLE IF NOT EXISTS first_table (id INTEGER, name VARCHAR(256));
