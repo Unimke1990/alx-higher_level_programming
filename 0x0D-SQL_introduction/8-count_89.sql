d28330d6e1b28060fa308a406fb12bfa03a4e4c1
Unimke1990 <agimunimke1990@gmail.com>
2024-02-23 05:30:24 -0800

    script that counts all records with a certain attribute in a table

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,5 @@
+-- file: 8-count_89.sql
+-- description: count all records with a certain attribute
+
+-- script that counts all records with a certain attribute in a table
+SELECT COUNT(*) FROM first_table WHERE id=89;
