ebe5eea23594ed6482174f5adf236548ae19bf85
Unimke1990 <agimunimke1990@gmail.com>
2024-02-23 06:02:29 -0800

    script that lists all records of a table in descending order

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- file: 10-top_score.sql
+-- description: list all records in descending order
+
+-- script that lists all records in descending order
+SELECT score, name FROM second_table ORDER BY score  DESC;
