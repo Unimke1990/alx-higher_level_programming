b196ef27173eec77e0fffc7da76d937467778091
Unimke1990 <agimunimke1990@gmail.com>
2024-02-27 02:46:01 -0800

    script that computes the score average of all records in the table

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,5 @@
+--File: 14-average.sql
+--Description: computes the score average of all records in the table
+
+-- script that computes the score average of all records in the table 
+SELECT AVG(score) AS average FROM hbtn_0c_0.second_table;
