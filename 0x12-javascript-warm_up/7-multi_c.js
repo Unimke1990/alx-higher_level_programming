9206fade84e1a84aef8d41827698bdcfa22f13a0
agim <agimagba1990@gmail.com>
2024-08-01 11:27:42 +0100

    cript that prints x times C is fun

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+let myNum = process.argv[2];
+let nums = parseInt(myNum);
+
+if (isNaN(nums)){
+	console.log('Missing number of occurrences')}
+let a = 0;
+
+while (a < nums) {console.log('C is fun')
+	a++;
+}
