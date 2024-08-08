8a963974b6b26b699c0427b03efbc9338429c5c0
Agim Unimke Agba <agimunimke1990@gmail.com>
2024-08-08 13:48:37 -0700

    sends a GET request with a header to the URL, and displays the body of the response

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#script that sends a GET request with a header and displays the body of the response
-curl -sH "X-School-User-Id" "$1"
+#sends a GET request with a header and displays the body of the response
+curl -sH "X-School-User-Id: 98" "$1"
